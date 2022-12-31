282a0de92126f4bce9dc56a80f9db8d24a5064c0
humbleseekre <nini2943@colorado.edu>
2022-12-30 19:49:54 -0800

    Update and rename parsering,py to parsering.py

--- parsering.py
+++ parsering.py
@@ -1,7 +1,21 @@
 #from https://docs.python.org/3/library/argparse.html
 #https://towardsdatascience.com/a-simple-guide-to-command-line-arguments-with-argparse-6824c30ab1c3
 
+# The argparse module provides a more sophisticated mechanism to process command line arguments. 
+# The following script extracts one or more filenames and an optional number of lines to be displayed:
+
 import argparse
+
+parser = argparse.ArgumentParser(
+    prog='top',
+    description='Show top lines from each file')
+parser.add_argument('filenames', nargs='+')
+parser.add_argument('-l', '--lines', type=int, default=10)
+args = parser.parse_args()
+print(args)
+
+# When run at the command line with python top.py --lines=5 alpha.txt beta.txt, the script sets args.lines to 5 
+# and args.filenames to ['alpha.txt', 'beta.txt'].
 
 def sum(num1, num2):
     print("the sum of two nums is {}".format(num1 + num2))
